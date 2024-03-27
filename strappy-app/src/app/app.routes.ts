3022c1c2413fbd65f8b49150ef977d13ef1cd8b1
David Perish <david@stonyhillsoftware.com>
2024-03-27 22:13:30 +0000

    Add feature pages and some routing

--- strappy-app/src/app/app.routes.ts
+++ strappy-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+/**
+ * Eager load the HomeComponent and lazy load the AboutComponent and ContactComponent.
+ */
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent, title: 'Home' },
+    { path: 'about', loadComponent: () => import('./features/about/about.component')
+                        .then(x => x.AboutComponent), title: 'About' },
+    { path: 'contact', loadComponent: () => import('./features/contact/contact.component')
+                        .then(x => x.ContactComponent), title: 'Contact' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
