d43efc71efc24c13b19b43786192245bca5b809d
David Perish <david@stonyhillsoftware.com>
2024-03-29 00:07:53 +0000

    Add dark mode toggle, and give the hero buttons some style

--- strappy-app/src/app/app.component.ts
+++ strappy-app/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgbDropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'strappy-app';
+
+  changeTheme(theme: string) {
+    const body = document.body as HTMLElement
+    body.setAttribute('data-bs-theme', theme)
+  }
+
 }
